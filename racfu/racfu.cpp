583314dcac82811a03cc7eb54c4c434703c1992c
Leonard Carcaramo <lcarcaramo@ibm.com>
2025-05-01 10:16:17 -0400

    Remove pythread from racfu.cpp
    Signed-off-by: Leonard Carcaramo <lcarcaramo@ibm.com>

--- racfu/racfu.cpp
+++ racfu/racfu.cpp
@@ -1,10 +1,6 @@
 #include "racfu.h"
 
-#include <pthread.h>
-
 #include "security_admin.hpp"
-
-pthread_mutex_t racfu_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 racfu_result_t *racfu(const char *request_json, int length, bool debug) {
   static racfu_result_t racfu_result = {nullptr, 0, nullptr, 0, nullptr};
