671d3bfa4a2cdc637e54e6f906a6b843c01880dd
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-02 05:39:46 +0200

    Fixed another spelling mistake

--- sear/key_map/key_map_group_connection.hpp
+++ sear/key_map/key_map_group_connection.hpp
@@ -31,7 +31,7 @@ const trait_key_mapping_t GROUP_CONNECTION_BASE_SEGMENT_MAP[]{
      TRAIT_TYPE_STRING, {false, false, false, false},
      },
     {
-     "base:connecion_last_used_time", "cgljtime",
+     "base:connection_last_used_time", "cgljtime",
      TRAIT_TYPE_STRING, {false, false, false, false},
      },
     {
