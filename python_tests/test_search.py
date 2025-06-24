27efa77fb903d27b2fefc828c8ba5d5c8799242a
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-24 18:42:44 +0200

    Added failing test case for search

---
+++ python_tests/test_search.py
@@ -0,0 +1,17 @@
+
+from helper import successful_return_codes
+
+# Import SEAR
+from sear import sear
+
+
+def test_search_resource_profiles_class_missing(create_dataset):
+    """This test is supposed to fail"""
+    add_result = sear(
+            {
+            "operation": "search", 
+            "admin_type": "resource", 
+            },
+        )
+    assert "errors" in str(add_result.result)
+    assert add_result.result["return_codes"] != successful_return_codes
