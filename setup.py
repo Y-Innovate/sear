b8fa793d0b14b50dec57688771c359995978521c
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-26 22:16:49 +0200

    Reverted to old build process temporarily

--- setup.py
+++ setup.py
@@ -1,76 +1,121 @@
+"""Build Python extension."""
+
+import json
+import os
+import subprocess
+from glob import glob
 from pathlib import Path
 
 from setuptools import Extension, setup
-from setuptools.command.build_ext import build_ext as _build_ext
-from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+from setuptools.command.build_ext import build_ext
 
 
-class CMakeExtension(Extension):
-    def __init__(self, name):
-        # don't invoke the original build_ext for this special extension
-        super().__init__(name, sources=[])
+def assemble(asm_file: str, asm_directory: Path) -> None:
+    """Assemble assembler code."""
+    obj_file = asm_file.split(".")[0] + ".o"
+    cwd = Path.cwd()
+    source_file = cwd / asm_directory / asm_file
+    obj_file = cwd / "artifacts" / obj_file
+
+    if obj_file.exists():
+        return
+
+    print(f"assembling {source_file}")
+
+    if not obj_file.parents[0].is_dir():
+        mkdir_command = f"mkdir {obj_file.parents[0]}"
+        print(mkdir_command)
+        subprocess.run(mkdir_command, shell=True, check=True)
+
+    assemble_command = (
+        f"as -mGOFF -I{source_file.parents[0]} -o {obj_file} {source_file}"
+    )
+    print(assemble_command)
+    subprocess.run(assemble_command, shell=True, check=True)
 
 
-class bdist_wheel(_bdist_wheel): # noqa: N801
-    def finalize_options(self):
-        super().finalize_options()
-
-        # marks built wheels as 'none-any' to allow installation on non-z/OS systems
-        self.root_is_pure = True
-
-
-class build_ext(_build_ext): # noqa: N801
-    def build_extension(self, ext) -> None:
-        self.build_cmake(ext)
-
-    def build_cmake(self, ext):
-        build_temp = Path(self.build_temp)
-        # ensure temporary build directory exists
-        build_temp.mkdir(parents=True, exist_ok=True)
-
-        extdir = Path(self.get_ext_fullpath(ext.name))
-        # ensure output directory exists
-        # probably not necessary, as cmake will create the directory during install
-        extdir.parent.mkdir(parents=True, exist_ok=True)
-
-        build_lib = Path(self.build_lib)
-        relative = extdir.relative_to(build_lib)
-
-        config = 'Debug' if self.debug else 'Release'
-
-        cmake_args = [
-            "--preset", "zos-pysear",
-            "-DSEAR_PYTHON_EXTENSION_PATH=" + str(relative),
-            "-DCMAKE_BUILD_TYPE=" + config,
-        ]
-
-        build_args = [
-            "--preset", "zos-pysear",
-            '--config', config,
-            '--', '-j4',
-        ]
-
-        install_args = [
-            # cmake --install does not work with --preset (yet)
-            # so build directory must be specified manually
-            "build/zos-pysear",
-            "--prefix=" + str(build_lib.absolute()),
-        ]
-
-        # configure cmake build directory
-        self.spawn(['cmake'] + cmake_args)
-        if not self.dry_run:
-            # first run
-            self.spawn(['cmake', '--build'] + build_args)
-            # then install built extension module
-            self.spawn(["cmake", "--install"] + install_args)
+def generate_json_schema_header() -> None:
+    """Generate SEAR JSON schema header."""
+    schema_absolute_path = Path.cwd() / "schema.json"
+    with open(schema_absolute_path) as f:
+        schema = json.dumps(json.load(f), separators=(",", ":"))
+    schema_header_absolute_path = Path.cwd() / "sear" / "sear_schema.hpp"
+    with open(schema_header_absolute_path, "w") as f:
+        f.write(
+            "\n".join(
+                [
+                    "#ifndef __SEAR_SCHEMA_H_",
+                    "#define __SEAR_SCHEMA_H_",
+                    "",
+                    f'#define SEAR_SCHEMA R"({schema})"_json',
+                    "",
+                    "#endif",
+                ],
+            ),
+        )
 
 
-setup(
-    name='pysear',
-    ext_modules=[CMakeExtension('sear._C')],
-    cmdclass={
-        'build_ext': build_ext,
-        "bdist_wheel": bdist_wheel,
-    },
-)
+class BuildExtensionWithAssemblerAndC(build_ext):
+    """Build Python extension that includes assembler code."""
+
+    def run(self):
+        os.environ["CC"] = "ibm-clang64"
+        os.environ["CFLAGS"] = "-std=c99"
+        os.environ["CXX"] = "ibm-clang++64"
+        os.environ["CXXFLAGS"] = "-std=c++14"
+        sear_source_path = Path("sear")
+        assemble("irrseq00.s", sear_source_path / "irrseq00")
+        super().run()
+
+
+def main():
+    """Python extension build entrypoint."""
+    cwd = Path.cwd()
+    # Use ZOPEN_ROOTFS to find OpenSSL and ZOSLIB.
+    if "ZOPEN_ROOTFS" not in os.environ:
+        raise RuntimeError(
+            "ZOPEN_ROOTFS is not set, but is required in order to "
+            + "find the zopen community distributions of of OpenSSL "
+            + "and ZOSLIB since they are build dependencies.\n"
+            + "You can find more information about setting up zopen "
+            + "community here: "
+            + "https://zopen.community/#/Guides/QuickStart?id=installing-zopen-package-manager",
+        )
+    assembled_object_path = cwd / "artifacts" / "irrseq00.o"
+    generate_json_schema_header()
+    setup_args = {
+        "ext_modules": [
+            Extension(
+                "sear._C",
+                sources=(
+                    glob("sear/**/*.cpp")
+                    + glob("sear/*.cpp")
+                    + glob("externals/json-schema-validator/*.cpp")
+                    + ["sear/python/_sear.c"]
+                ),
+                include_dirs=(
+                    glob("sear/**/")
+                    + [
+                        "sear",
+                        "externals/json",
+                        "externals/json-schema-validator",
+                        os.environ["ZOPEN_ROOTFS"] + "/usr/local/include",
+                    ]
+                ),
+                extra_link_args=[
+                    "-m64",
+                    "-Wl,-b,edit=no",
+                    "-Wl," + os.environ["ZOPEN_ROOTFS"] + "/usr/local/lib/libcrypto.a",
+                    "-Wl," + os.environ["ZOPEN_ROOTFS"] + "/usr/local/lib/libssl.a",
+                    "-Wl," + os.environ["ZOPEN_ROOTFS"] + "/usr/local/lib/libzoslib.a",
+                ],
+                extra_objects=[f"{assembled_object_path}"],
+            ),
+        ],
+        "cmdclass": {"build_ext": BuildExtensionWithAssemblerAndC},
+    }
+    setup(**setup_args)
+
+
+if __name__ == "__main__":
+    main()
