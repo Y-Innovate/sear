28963a358b4098a4d435c6df16c86d029e35ae4d
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-07-03 22:20:53 +0200

    Added more failing test cases to permits

--- python_tests/test_permit.py
+++ python_tests/test_permit.py
@@ -38,6 +38,22 @@ def test_add_dataset_permit_missing_admin_type(create_user, create_dataset):
     assert "errors" in str(add_result.result)
     assert add_result.result["return_codes"] != successful_return_codes
 
+def test_add_dataset_permit_missing_operation(create_user, create_dataset):
+    """This test is supposed to fail"""
+    add_result = sear(
+            {
+            "admin_type": "permission", 
+            "dataset": create_dataset,
+            "userid": create_user,
+            "generic": True,
+            "traits": {
+                "base:access": "READ",
+            },
+            },
+        )
+    assert "errors" in str(add_result.result)
+    assert add_result.result["return_codes"] != successful_return_codes
+
 def test_add_dataset_permit_missing_userid(create_dataset):
     """This test is supposed to fail"""
     add_result = sear(
@@ -71,6 +87,23 @@ def test_add_resource_permit(create_user, create_resource):
         )
     assert "errors" not in str(add_result.result)
     assert add_result.result["return_codes"] == successful_return_codes
+
+def test_add_resource_permit_missing_operation(create_user, create_resource):
+    """This test is supposed to fail"""
+    profile_name, class_name = create_resource
+    add_result = sear(
+            {
+            "admin_type": "permission", 
+            "resource": profile_name,
+            "class": class_name,
+            "userid": create_user,
+            "traits": {
+                "base:access": "READ",
+            },
+            },
+        )
+    assert "errors" in str(add_result.result)
+    assert add_result.result["return_codes"] != successful_return_codes
 
 def test_add_resource_permit_missing_admin_type(create_user, create_resource):
     """This test is supposed to fail"""
