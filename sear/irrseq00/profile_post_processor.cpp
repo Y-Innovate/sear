2a82b63e0b77f013595a0a5e1bcdf0ec1eb87037
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-10-01 20:47:51 +0200

    test

--- sear/irrseq00/profile_post_processor.cpp
+++ sear/irrseq00/profile_post_processor.cpp
@@ -275,7 +275,14 @@ std::string ProfilePostProcessor::postProcessKey(const char *p_source_key,
 
 std::string ProfilePostProcessor::decodeEBCDICBytes(const char *p_ebcdic_bytes,
                                                     int length) {
-  std::string utf8_string = toUTF8(p_ebcdic_bytes);
+  auto ebcdic_bytes_unique_ptr          = std::make_unique<char[]>(length);
+  ebcdic_bytes_unique_ptr.get()[length] = 0;
+  // Decode bytes
+  std::strncpy(ebcdic_bytes_unique_ptr.get(), p_ebcdic_bytes, length);
+  
+  std::string ebcdic_string = std::string(ebcdic_bytes_unique_ptr.get());
+
+  std::string utf8_string = toUTF8(ebcdic_string);
 
   size_t end = utf8_string.find_last_not_of(" ");
 
