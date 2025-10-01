8838776e3b043d665113994b0bf7198be5e8f31f
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-10-01 21:37:33 +0200

    removed debug code

--- sear/conversion.cpp
+++ sear/conversion.cpp
@@ -50,8 +50,7 @@ std::string convert(std::string input, std::string_view inputCodepage, std::stri
 
 std::string SEAR::toUTF8(std::string input, std::string_view codepage) {
     std::string fromCode{codepage};
-   Logger::getInstance().debug("Converting from " + fromCode + " to UTF-8");
-   return convert(input,codepage,"UTF-8");
+    return convert(input,codepage,"UTF-8");
 }
 
 std::string SEAR::fromUTF8(std::string input) {
